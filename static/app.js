789f05596e647931be8662f44171caa76c6d3184
derekjohnson7 <derek.johnson7@yahoo.com>
2024-02-21 21:14:31 -0600

    added file

---
+++ static/app.js
@@ -0,0 +1,24 @@
+
+import apiKey from './config.js';
+
+let url = `https://api.sportsdata.io/v3/nhl/scores/json/Players?key=${apiKey}`
+
+d3.json(url).then(function(data){
+
+    console.log(data)
+
+    let dropDownmenu = d3.select("#selDataset")
+    
+    let formattedNames = []
+
+    for (let i = 0; i < data.length; i++) {
+        let fullName = data[i].FirstName + " " + data[i].LastName;
+        formattedNames.push(fullName);
+    }
+    
+    formattedNames.sort();
+    
+    for (let i = 0; i < formattedNames.length; i++) {
+        dropDownmenu.append('option').text(formattedNames[i]);
+    }
+});
---
+++ static/config.js
@@ -0,0 +1,4 @@
+const apiKey = "fff8f675f5004f1b844d8fdaf9cf7c4e";
+const sportsRadarAPI = "gt4e65wtdwvtkx2vs7zyszys";
+export default apiKey;
+
