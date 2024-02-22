85919e5095f3f0b42ef946a52182cd5f46d1f916
derekjohnson7 <derek.johnson7@yahoo.com>
2024-02-22 11:50:13 -0600

    added .gitignore

--- static/config.js
+++ static/config.js
@@ -1,4 +1,5 @@
-const apiKey = "fff8f675f5004f1b844d8fdaf9cf7c4e";
-const sportsRadarAPI = "gt4e65wtdwvtkx2vs7zyszys";
-export default apiKey;
+const apiKeys = {
+apiKey1: "fff8f675f5004f1b844d8fdaf9cf7c4e",
+apikey2: "gt4e65wtdwvtkx2vs7zyszys"}
+export default apiKeys;
 
